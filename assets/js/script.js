35b9cfcc5318f4e80c821d029870e3600b39c11b
matteofalco1986 <matteo.falco1986@gmail.com>
2023-12-09 12:59:09 +0100

    Add product page link improve

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,19 @@ let products = [];
 const timeouts = [0, 10, 50, 100, 300, 500, 750, 1000, 1500, 2000]
 const homePageRow = document.querySelector('.homePageRow');
 const productPageRow = document.querySelector('.productPageRow');
-const insertFormSection = document.querySelector("#insertForm");
+const insertFormSection = document.querySelector(".insertFormContainer");
+const productPage = document.querySelector('#productPage');
 const addNewItemBtn = document.querySelector('addNewItem');
 
 
 // Gets data from fake e-commerce API and populates products array
 getFakeData(fakeData);
 
-// Gets all data in API
+// Gets all data from API to the Home Page
 getDataToHomePage("");
 
 // Makes info buttons clickable and able to open product page
-setTimeout(openInfoPage, timeouts[7]);
+setTimeout(openProductPage, timeouts[7]);
 
 // Prints products in the array
 setTimeout(() => {
@@ -24,36 +25,10 @@ setTimeout(() => {
 }, timeouts[9]);
 
 
+addNewItemBtn.addEventListener('click', toInsertForm)
 
-// setTimeout(() => postData(products[5]), 3000)
-populateInsertForm();
-// Select all the items in the page
-// The red button must reset the form
-allFieldsElements = document.querySelectorAll('input, textarea');
-resetBtn = document.querySelector('.resetForm');
-addProductBtn = document.querySelector('.addProduct');
-// Resets all fields
-resetBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    allFieldsElements.forEach((element) => {
-        element.value = '';
-    });
-})
-// The green button must create a new object
-addProductBtn.addEventListener('click', (event)=>{
-    event.preventDefault();
-    let productName = document.querySelector('#name');
-    let brand = document.querySelector('#brand');
-    let imageUrl = document.querySelector('#imageUrl');
-    let price = document.querySelector('#price');
-    let description = document.querySelector('#description');
-    // Take info in the objects from the fields and create object
-    itemToAdd = new createArticle(productName.value, description.value, brand.value, imageUrl.value, parseFloat(price.value));
-    console.log(itemToAdd);
-    setTimeout(() => postData(itemToAdd), 0);
-})
 
-// Make a post request with the object passed as argument
+
 // Update home page with new data
 // Make page disappear
 // Make home page visible
@@ -80,6 +55,7 @@ async function getDataToHomePage(_id) {
     }
 }
 
+// GET DATA TO PRODUCT PAGE
 async function getDataToProductPage(_id) {
     try {
         const response = await fetch(endPointUrl + _id, {
@@ -98,6 +74,43 @@ async function getDataToProductPage(_id) {
     } catch (error) {
         console.log("Error: ", error);
     }
+}
+
+// POPULATES PRODUCT PAGE WITH DATA
+function populateProductPage(singleArticle) {
+    // Add single product to HTML
+    productPageRow.innerHTML = '';
+    productPageRow.innerHTML = `                  
+                        <div class="col">
+                            <img src="${singleArticle.imageUrl}" alt=${singleArticle.name}>
+                        </div>
+                        <div class="col">
+                            <h2>${singleArticle.name}</h2>
+                            <h3>${singleArticle.brand}</h3>
+                            <p class="productDescription">${singleArticle.description}</p>
+                            <p>${singleArticle.price}</p>
+                            <a href="#" class="modifyBtn btn btn-danger">Modifica</a>
+                        </div>
+                    `
+}
+
+// OPENS PRODUCT PAGE
+function openProductPage() {
+    const findOutMoreLinks = document.querySelectorAll('.findOutMoreBtn');
+    findOutMoreLinks.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            let productCard = link.closest(".card");
+            // Make a request to the server using the id stored in the data-id attribute
+            getDataToProductPage(productCard.dataset.id)
+            // Hides previous page
+            setTimeout(() => {
+                document.querySelector('.visible').classList.remove('visible');
+                document.querySelector('#productPage').classList.add('visible');
+            }, timeouts[5])
+            setTimeout(() => backToHomePage(0), timeouts[7])
+        });
+    });
 }
 
 // POST DATA
@@ -224,52 +237,45 @@ function populateHomePage(articles) {
     }
 }
 
-function openInfoPage() {
-    const findOutMoreLinks = document.querySelectorAll('.findOutMoreBtn');
-    findOutMoreLinks.forEach((link) => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            let productCard = link.closest(".card");
-            // Make a request to the server using the id stored in the data-id attribute
-
-            getDataToProductPage(productCard.dataset.id)
-            // Hides previous page
-            setTimeout(() => {
-                document.querySelector('.visible').classList.remove('visible');
-                document.querySelector('#productPage').classList.add('visible');
-            }, timeouts[5])
-            setTimeout(backToHomePage, timeouts[7])
-        });
-    });
-}
 
 
-function populateProductPage(singleArticle) {
-    // Add single product to HTML
-    productPageRow.innerHTML = '';
-    productPageRow.innerHTML = `                  
-                        <div class="col">
-                            <img src="${singleArticle.imageUrl}" alt=${singleArticle.name}>
-                        </div>
-                        <div class="col">
-                            <h2>${singleArticle.name}</h2>
-                            <h3>${singleArticle.brand}</h3>
-                            <p class="productDescription">${singleArticle.description}</p>
-                            <p>${singleArticle.price}</p>
-                            <a href="#" class="modifyBtn btn btn-danger">Modifica</a>
-                        </div>
-                    `
-}
-
-function backToHomePage() {
-    document.querySelector(".homePageBtn").addEventListener('click', () => {
+// MAKES HOME PAGE VISIBLE AGAIN
+function backToHomePage(index) {
+    document.querySelectorAll(".homePageBtn")[index].addEventListener('click', () => {
         document.querySelector(".visible").classList.remove('visible');
         document.querySelector('#homePage').classList.add('visible');
     })
 };
 
 function toInsertForm() {
-    // Inject the content to the HTML
+    populateInsertForm();
+    // Select all the items in the page
+    // The red button must reset the form
+    allFieldsElements = document.querySelectorAll('input, textarea');
+    resetBtn = document.querySelector('.resetForm');
+    addProductBtn = document.querySelector('.addProduct');
+    // Resets all fields
+    resetBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        allFieldsElements.forEach((element) => {
+            element.value = '';
+        });
+    })
+    // The green button must create a new object
+    addProductBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        let productName = document.querySelector('#name');
+        let brand = document.querySelector('#brand');
+        let imageUrl = document.querySelector('#imageUrl');
+        let price = document.querySelector('#price');
+        let description = document.querySelector('#description');
+        // Take info in the objects from the fields and create object
+        itemToAdd = new createArticle(productName.value, description.value, brand.value, imageUrl.value, parseFloat(price.value));
+        // Make a post request with the object passed as argument
+        console.log(itemToAdd);
+        setTimeout(() => postData(itemToAdd), timeouts[9]);
+    })
+    setTimeout(() => backToHomePage(1), timeouts[1])
 
     // Select all the items in the page
     // The red button must reset the form
@@ -285,12 +291,10 @@ function toInsertForm() {
 function populateInsertForm() {
     insertFormSection.innerHTML = '';
     const textToInject = `
-                <h2 class="formTitle text-center">INSERT A NEW PRODUCT</h2>
-                <div class="insertFormContainer">
                     <form action="" class="insertForm">
                         <div class="inputField d-flex">
                             <label for="name">Product name</label>
-                            <input type="text" name="name" id="name" placeholder="Type in the name ofthe product">
+                            <input type="text" name="name" id="name" placeholder="Type in the name of the product">
                         </div>
                         <div class="inputField d-flex">
                             <label for="brand">Brand</label>
@@ -319,14 +323,19 @@ function populateInsertForm() {
     insertFormSection.innerHTML = textToInject;
 }
 
-// CREAZIONE
-// Quando clicco nuovo, deve comparire una finestra form con una serie di campi da compilare
-// Cliccando sul prodotto deve aprirsi la pagina del form per l'aggiunta alla API
-// Il bottone di reset del form, deve ripristinare i campi ai valori di default del prodotto da aggiungere dall'array PRODUCTS.
-// Il bottone crea risorsa, deve caricare il prodotto sulla API tramite una richiesta POST.
-
 //MODIFICA
 // Il form deve mostrarmi i campi precompilati di titolo, testo, marca e prezzo da poter cambiare
 // Nella schermata di modifica, la pagina deve mostrarmi quanto è presente nei campi, con la possibilità di modificarli. Il tipo di richiesta sarà un PUT.
 // La schermata deve darmi la possibilità di cancellare la risorsa dalla API.
 // Il pulsante di reset del form deve ripristinare i valori dei campi a quelli memorizzati nella API
+
+
+
+
+
+
+// DELETE BEFORE STARTING
+
+// setTimeout(() => {
+//     deleteData("6574496c2c6a0d00184959d3")
+// }, 3000)
